7befebfcaedbaa7a18898756ec473ae18a18e5aa
Victor Gonzalez <victor.gonzalez.garrido@est.fib.upc.edu>
2017-03-26 16:48:59 +0200

    primers tests simples creats

--- app/src/androidTest/java/pes/gogreenapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/pes/gogreenapp/ExampleInstrumentedTest.java
@@ -2,25 +2,91 @@ package pes.gogreenapp;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("pes.gogreenapp", appContext.getPackageName());
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import pes.gogreenapp.Activities.RewardsList;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ExampleInstrumentedTest {
+
+
+    @Rule
+    public ActivityTestRule<RewardsList> mActivityRule = new ActivityTestRule<>(
+            RewardsList.class);
+
+    @Test
+    public void PuntosAsc() {
+
+        onView(withId(R.id.orderPointsButton))
+                .perform(scrollTo())
+                .perform(click())
+                .check(matches(withText("PUNTOS ↑")));
+        onView(withId(R.id.orderPointsButton))
+                .perform(scrollTo())
+                .perform(click())
+                .check(matches(withText("PUNTOS ↓")));
+        onView(withId(R.id.orderDateButton))
+                .perform(scrollTo())
+                .perform(click());
+
+        onView(withId(R.id.orderPointsButton))
+                .check(matches(withText("PUNTOS")));
+    }
+    @Test
+    public void PuntosDesc() {
+
+        onView(withId(R.id.orderPointsButton))
+                .perform(scrollTo())
+                .perform(click())
+                .check(matches(withText("PUNTOS ↑")));
+        onView(withId(R.id.orderPointsButton))
+                .perform(scrollTo())
+                .perform(click())
+                .check(matches(withText("PUNTOS ↓")));
+    }
+    @Test
+    public void NoPuntos() {
+
+        onView(withId(R.id.orderPointsButton))
+                .perform(scrollTo())
+                .perform(click())
+                .check(matches(withText("PUNTOS ↑")));
+        onView(withId(R.id.orderPointsButton))
+                .perform(scrollTo())
+                .perform(click())
+                .check(matches(withText("PUNTOS ↓")));
+        onView(withId(R.id.orderDateButton))
+                .perform(scrollTo())
+                .perform(click());
+
+        onView(withId(R.id.orderPointsButton))
+                .check(matches(withText("PUNTOS")));
+     /*   onView(withRecyclerView(R.id.rv)
+                .atPositionOnView(1, R.id.ofElementYouWantToCheck))
+                .check(matches(withText("Test text")));*/
     }
 }
