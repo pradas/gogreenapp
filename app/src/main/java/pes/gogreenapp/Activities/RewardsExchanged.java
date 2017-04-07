fb8f7cc876827ffa64159ff47a15df5bba31b007
Adrian Borrego <adrian.borrego@est.fib.upc.edu>
2017-04-07 17:50:25 +0200

    Inicio de las activities y adapter de Rewards Canjeadas

--- app/src/main/java/pes/gogreenapp/Activities/RewardsExchanged.java
+++ app/src/main/java/pes/gogreenapp/Activities/RewardsExchanged.java
@@ -1,8 +1,20 @@
 package pes.gogreenapp.Activities;
 
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import pes.gogreenapp.Adapters.RewardsExchangedAdapter;
+
 /**
  * Created by Adry on 07/04/2017.
  */
 
-public class RewardsExchanged {
+public class RewardsExchanged extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RewardsExchangedAdapter adapter;
+    private SwipeRefreshLayout swipeContainer;
+    private String TAG = RewardsExchanged.class.getSimpleName();
+
 }
---
+++ app/src/main/java/pes/gogreenapp/Adapters/RewardsExchangedAdapter.java
@@ -0,0 +1,27 @@
+package pes.gogreenapp.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Adry on 07/04/2017.
+ */
+
+public class RewardsExchangedAdapter extends RecyclerView.Adapter<RewardsExchangedAdapter.ViewHolder> {
+
+
+    @Override
+    public RewardsExchangedAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RewardsExchangedAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
