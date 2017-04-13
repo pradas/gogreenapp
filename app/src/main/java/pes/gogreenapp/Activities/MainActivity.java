eaa1af9d6f15dda53d4d38ac202e944d1373c640
Albert <albert.diaz.benitez@est.fib.upc.edu>
2017-04-13 12:47:27 +0200

    Second version of NavigationDrawer (functional, need to implement actions on click)

--- app/src/main/java/pes/gogreenapp/Activities/MainActivity.java
+++ app/src/main/java/pes/gogreenapp/Activities/MainActivity.java
@@ -3,11 +3,18 @@ package pes.gogreenapp.Activities;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.Console;
 
 import pes.gogreenapp.Fragments.RewardsList;
 import pes.gogreenapp.R;
@@ -19,28 +26,85 @@ import pes.gogreenapp.R;
 public class MainActivity extends Activity {
     private String[] myDrawerTitles;
     private CharSequence myTitle;
-    private ActionBarDrawerToggle myDrawerToggle;
     private CharSequence myDrawerTitle;
     private DrawerLayout myDrawerLayout;
     private ListView myDrawerList;
+    private ActionBarDrawerToggle myDrawerToggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myTitle = myDrawerTitle = getTitle();
         myDrawerTitles = getResources().getStringArray(R.array.navigation_drawer_items);
         myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         myDrawerList = (ListView) findViewById(R.id.left_drawer);
 
+        ArrayAdapter<String> aux = new ArrayAdapter<String>(
+                this,
+                R.layout.drawer_list_item,
+                myDrawerTitles);
+
+        Log.i("INFO", aux.toString());
+
         // Set the adapter for the list view
-        myDrawerList.setAdapter(new ArrayAdapter<>(this,
-                R.layout.activity_navigation_drawer, myDrawerTitles));
+        myDrawerList.setAdapter(aux);
         // Set the list's click listener
         myDrawerList.setOnItemClickListener((parent, view, position, id) -> {
             selectItem(position);
         });
 
+        myDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                myDrawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(myTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(myDrawerTitle);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        myDrawerLayout.addDrawerListener(myDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        myDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        myDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (myDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -65,9 +129,13 @@ public class MainActivity extends Activity {
         myDrawerLayout.closeDrawer(myDrawerList);
     }
 
+    /* Called whenever we call invalidateOptionsMenu() */
     @Override
-    public void setTitle(CharSequence title) {
-        myTitle = title;
-        getActionBar().setTitle(myTitle);
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = myDrawerLayout.isDrawerOpen(myDrawerList);
+        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
+
 }
