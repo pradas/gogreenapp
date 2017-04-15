2170004d15ea0aa5287b2b5f01909e081c1cd205
Albert <albert.diaz.benitez@est.fib.upc.edu>
2017-04-15 19:54:14 +0200

    Refactor & Javadocs of the Adapters

--- app/src/main/java/pes/gogreenapp/Adapters/RewardsListAdapter.java
+++ app/src/main/java/pes/gogreenapp/Adapters/RewardsListAdapter.java
@@ -17,26 +17,40 @@ import pes.gogreenapp.Objects.Reward;
 /**
  * Created by Albert on 19/03/2017.
  */
-
 public class RewardsListAdapter extends RecyclerView.Adapter<RewardsListAdapter.ViewHolder> {
-
     private List<Reward> rewards;
 
+    /**
+     * Constructor that set the List of Rewards.
+     *
+     * @param rewards non-null List of the Rewards.
+     */
     public RewardsListAdapter(List<Reward> rewards) {
         this.rewards = rewards;
     }
 
-    public void setRewards(List<Reward> rewards) { this.rewards = rewards; }
+    /**
+     * Setter of the Rewards List.
+     *
+     * @param rewards non-null List of the Rewards.
+     */
+    public void setRewards(List<Reward> rewards) {
+        this.rewards = rewards;
+    }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView points;
+        TextView date;
+        TextView category;
+        ImageButton fav;
 
-        public TextView title;
-        public TextView points;
-        public TextView date;
-        public TextView category;
-        public ImageButton fav;
-
-        public ViewHolder(View itemView) {
+        /**
+         * Constructor of the View Holder that sets all the items.
+         *
+         * @param itemView valid View where to construct.
+         */
+        ViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.rewardTitle);
             points = (TextView) itemView.findViewById(R.id.rewardPoints);
@@ -46,13 +60,30 @@ public class RewardsListAdapter extends RecyclerView.Adapter<RewardsListAdapter.
         }
     }
 
+
+    /**
+     * Obtains the LayoutInflater from the given context and usse it to create a new ViewHolder.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     */
     @Override
     public RewardsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rewards_list_cardview, parent, false);
-        ViewHolder viewHolder = new ViewHolder(v);
-        return viewHolder;
+        return new ViewHolder(v);
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the {@link ViewHolder#itemView} to reflect the item at the given
+     * position.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(final RewardsListAdapter.ViewHolder holder, int position) {
         holder.title.setText(rewards.get(position).getTitle());
@@ -60,21 +91,22 @@ public class RewardsListAdapter extends RecyclerView.Adapter<RewardsListAdapter.
         Date d = rewards.get(position).getEndDate();
         holder.date.setText(new SimpleDateFormat("dd/MM/yyyy").format(d));
         holder.category.setText(rewards.get(position).getCategory());
-        holder.fav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (holder.fav.getTag().equals("favorite")) {
-                    holder.fav.setImageResource(R.mipmap.favoritefilled);
-                    holder.fav.setTag("favoritefilled");
-                } else {
-                    holder.fav.setImageResource(R.mipmap.favorite);
-                    holder.fav.setTag("favorite");
-                }
+        holder.fav.setOnClickListener(v -> {
+            if (holder.fav.getTag().equals("favorite")) {
+                holder.fav.setImageResource(R.mipmap.favoritefilled);
+                holder.fav.setTag("favoritefilled");
+            } else {
+                holder.fav.setImageResource(R.mipmap.favorite);
+                holder.fav.setTag("favorite");
             }
         });
     }
 
-
+    /**
+     * Returns the total number of rewards in the data set held by the adapter.
+     *
+     * @return The total number of rewards in this adapter.
+     */
     @Override
     public int getItemCount() {
         return rewards.size();
