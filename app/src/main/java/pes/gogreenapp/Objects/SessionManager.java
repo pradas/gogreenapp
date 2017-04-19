65e8a92126e0c3e7304347a34496052bbe1ce34e
Jorge Alvarez <jorge@path.cat>
2017-04-19 17:27:06 +0200

    Checked out SessionManger from origin/master

---
+++ app/src/main/java/pes/gogreenapp/Objects/SessionManager.java
@@ -0,0 +1,135 @@
+package pes.gogreenapp.Objects;
+
+import java.util.HashMap;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import pes.gogreenapp.Activities.LoginActivity;
+import pes.gogreenapp.Activities.MainActivity;
+
+public class SessionManager {
+    // Shared Preferences
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    Editor editor;
+
+    // Context
+    Context _context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "GoGreenPref";
+
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+    // User name (make variable public to access from outside)
+    public static final String KEY_NAME = "name";
+
+    // Token (make variable public to access from outside)
+    public static final String KEY_TOKEN = "token";
+
+    // Constructor
+    public SessionManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    /**
+     * Create login_activity session
+     */
+    public void createLoginSession(String name, String token) {
+        // Storing login_activity value as TRUE
+        editor.putBoolean(IS_LOGIN, true);
+
+        // Storing name in pref
+        editor.putString(KEY_NAME, name);
+
+        // Storing email in pref
+        editor.putString(KEY_TOKEN, token);
+
+        // commit changes
+        editor.commit();
+    }
+
+    /**
+     * Check login_activity method wil check user login_activity status
+     * If false it will redirect user to login_activity page
+     * Else won't do anything
+     */
+    public void checkLogin() {
+        // Check login_activity status
+        if (!this.isLoggedIn()) {
+            // user is not logged in redirect him to LoginActivity Activity
+            Intent i = new Intent(_context, LoginActivity.class);
+            // Closing all the Activities
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Staring LoginActivity Activity
+            _context.startActivity(i);
+        }
+
+    }
+
+
+    /**
+     * Get stored session data
+     */
+    public HashMap<String, String> getUserDetails() {
+        HashMap<String, String> user = new HashMap<String, String>();
+        // user name
+        user.put(KEY_NAME, pref.getString(KEY_NAME, null));
+
+        // user email id
+        user.put(KEY_TOKEN, pref.getString(KEY_TOKEN, null));
+
+        // return user
+        return user;
+    }
+
+    /**
+     * Clear session details
+     */
+    public void logoutUser() {
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+
+        // After logout redirect user to Loing Activity
+        Intent i = new Intent(_context, LoginActivity.class);
+        // Closing all the Activities
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Add new Flag to start new Activity
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Staring LoginActivity Activity
+        _context.startActivity(i);
+    }
+
+    /**
+     * Quick check for login_activity
+     **/
+    // Get LoginActivity State
+    public boolean isLoggedIn() {
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+
+    public String getToken() {
+        return pref.getString(KEY_TOKEN, "");
+    }
+
+    public String getUsername() {
+        return pref.getString(KEY_NAME, "");
+    }
+}
