aa5276aad4c9c932241d4fa5728ec5bf113a1416
Jorge Alvarez <jorge.alvarez.fernandez@est.fib.upc.edu>
2017-04-21 02:40:00 +0200

    Mensajes de error mas precisos, JavaDoc en la activity aplicado

--- app/src/main/java/pes/gogreenapp/Activities/FormUserActivity.java
+++ app/src/main/java/pes/gogreenapp/Activities/FormUserActivity.java
@@ -4,8 +4,6 @@ import android.app.DatePickerDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -13,38 +11,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
+import java.util.regex.Pattern;
 
-import pes.gogreenapp.Adapters.RewardsAdapter;
 import pes.gogreenapp.Handlers.HttpHandler;
-import pes.gogreenapp.Objects.Reward;
 import pes.gogreenapp.R;
 
 /**
@@ -60,6 +42,7 @@ public class FormUserActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        findViewById(R.id.editFechaNacimiento).setOnKeyListener(null);
         final ImageButton pickDate = (ImageButton) findViewById(R.id.datePickerButton);
         final TextView textView = (TextView) findViewById(R.id.editFechaNacimiento);
 
@@ -73,7 +56,7 @@ public class FormUserActivity extends AppCompatActivity {
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 // myCalendar.add(Calendar.DATE, 0);
-                String myFormat = "yyyy-MM-dd"; //In which you need put here
+                String myFormat = "dd-MM-yyyy"; //In which you need put here
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.FRANCE);
                 textView.setText(sdf.format(myCalendar.getTime()));
             }
@@ -84,7 +67,7 @@ public class FormUserActivity extends AppCompatActivity {
         pickDate.setOnClickListener((View v) -> {
                 // TODO Auto-generated method stub
                 final Calendar c = Calendar.getInstance();
-                mYear = c.get(Calendar.YEAR);
+                mYear = c.get(Calendar.YEAR)-18;
                 mMonth = c.get(Calendar.MONTH);
                 mDay = c.get(Calendar.DAY_OF_MONTH);
 
@@ -105,15 +88,17 @@ public class FormUserActivity extends AppCompatActivity {
 
                                 if (dayOfMonth > mDay && year == mYear && monthOfYear == mMonth)
                                     view.updateDate(mYear, mMonth, mDay);
-
-                                textView.setText(dayOfMonth + "-"
-                                        + (monthOfYear + 1) + "-" + year);
+                                String sDayOfMonth = String.format("%02d",dayOfMonth);
+                                String sMonthOfYear = String.format("%02d",monthOfYear + 1);
+                                textView.setText(sDayOfMonth + "-"
+                                        + sMonthOfYear + "-" + year);
 
                             }
                         }, mYear, mMonth, mDay);
-                dpd.getDatePicker().setMaxDate(System.currentTimeMillis());
-                dpd.show();
-
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(mYear,mMonth,mDay);
+            dpd.getDatePicker().setMaxDate(calendar.getTimeInMillis());
+            dpd.show();
         });
     }
         @Override
@@ -124,6 +109,9 @@ public class FormUserActivity extends AppCompatActivity {
         }
 
     @Override
+    /**
+     * Override default onStop to apply a interruption of Petitions
+     */
     protected void onStop() {
         super.onStop();
         if (mRequestQueue != null) {
@@ -136,26 +124,44 @@ public class FormUserActivity extends AppCompatActivity {
         switch(item.getItemId()){
             case R.id.action_favorite:
                 String password = ((TextView) findViewById(R.id.editContraseña)).getText().toString();
-                if(!password.isEmpty() && password.equals(((TextView) findViewById(R.id.editContraseñaConfirmar)).getText().toString())) {
-                    String username = ((TextView) findViewById(R.id.editUsername)).getText().toString();
-                    String name = ((TextView) findViewById(R.id.editName)).getText().toString();
-                    String email = ((TextView) findViewById(R.id.editEmail)).getText().toString();
-                    String birthdayDate = ((TextView) findViewById(R.id.editFechaNacimiento)).getText().toString();
-                    new PostMethod().execute(URLPetition,username,name,email,password,birthdayDate);
-                }else
-                    Toast.makeText(this,"Error contraseña no valida", Toast.LENGTH_LONG).show();
+                String username = ((TextView) findViewById(R.id.editUsername)).getText().toString();
+                String name = ((TextView) findViewById(R.id.editName)).getText().toString();
+                String email = ((EditText) findViewById(R.id.editEmail)).getText().toString();
+                String birthdayDate = ((TextView) findViewById(R.id.editFechaNacimiento)).getText().toString();
+                if(username.isEmpty() || name.isEmpty() || email.isEmpty() || birthdayDate.isEmpty())
+                    Toast.makeText(this, "Faltan datos por rellenar", Toast.LENGTH_LONG).show();
+                else {
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                        Toast.makeText(this, "Email no valido", Toast.LENGTH_LONG).show();
+                    } else if (!password.isEmpty() && password.equals(((TextView) findViewById(R.id.editContraseñaConfirmar)).getText().toString())) {
+                        new PostMethod().execute(URLPetition, username, name, email, password, birthdayDate);
+                    } else
+                        Toast.makeText(this, "Error contraseña no valida", Toast.LENGTH_LONG).show();
+                    }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
-
+    /**
+     * Asynchronous Task for the petition POST to send a petition of register an User
+     */
     private class PostMethod extends AsyncTask<String, Void, Void> {
         @Override
+        /**
+         * Execute Asynchronous Task calling the url passed by parameter 0.
+         *
+         * @param params params[0] is the petition url,
+         *               params[1] is the username,
+         *               params[2] is the name of the user
+         *               params[3] is the email
+         *               params[4] is the password
+         *               params[5] is the birthday date
+         * @return void when finished
+         */
         protected Void doInBackground(String... params) {
             mRequestQueue = Volley.newRequestQueue(getApplicationContext());
             HashMap<String,String> impl = new HashMap<>();
-            // 'name', 'email', 'password', 'username', 'role_id'
             impl.put("username",params[1]);
             impl.put("name",params[2]);
             impl.put("email",params[3]);
@@ -163,6 +169,12 @@ public class FormUserActivity extends AppCompatActivity {
             impl.put("birth_date",params[5]);
             String result = new HttpHandler().makeServiceCall(params[0],"POST" ,impl,"");
             Log.i(TAG, "Response from url: " + result);
+            if(result.isEmpty()){
+                Toast.makeText(getApplicationContext(),"No se ha conseguido enviar los datos",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(getApplicationContext(),"Usuario creado",Toast.LENGTH_LONG).show();
+                //TODO Change Location
+            }
             return null;
         }
     }
