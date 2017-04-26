0d78c7ab5ac1c2ec465c08a01ce514fd1461e54b
Adrian Borrego <adrian.borrego@est.fib.upc.edu>
2017-04-27 01:19:51 +0200

    Tests Canjear Puntos empezados

---
+++ app/src/androidTest/java/pes/gogreenapp/MyViewAction.java
@@ -0,0 +1,33 @@
+package pes.gogreenapp;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+/**
+ * Created by Adrian on 27/04/2017.
+ */
+
+public class MyViewAction {
+    public static ViewAction clickChildViewWithId(final int id) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Click on a child view with specified id.";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View v = view.findViewById(id);
+                v.performClick();
+            }
+        };
+    }
+}
