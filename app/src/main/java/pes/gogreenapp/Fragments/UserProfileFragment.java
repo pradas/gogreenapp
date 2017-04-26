63d902b617293134e2e64a7641f6a816f85f7037
dga93 <dgalcaide93@gmail.com>
2017-04-26 19:00:26 +0200

    rename fragment container xml

--- app/src/main/java/pes/gogreenapp/Fragments/UserProfileFragment.java
+++ app/src/main/java/pes/gogreenapp/Fragments/UserProfileFragment.java
@@ -55,7 +55,7 @@ public class UserProfileFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_profile_container_fragment, container, false);
+        return inflater.inflate(R.layout.user_profile_fragment, container, false);
     }
 
     @Override
