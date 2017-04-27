65ec7820086c5dc9452058f7c1846f96142d79bc
Jorge Alvarez <jorge.alvarez.fernandez@est.fib.upc.edu>
2017-04-28 01:10:54 +0200

    Pequeno problema con login encontrado, miniparche para evitar problemillas, preguntar antes de hacer merge

--- app/src/main/java/pes/gogreenapp/Fragments/LoginFragment.java
+++ app/src/main/java/pes/gogreenapp/Fragments/LoginFragment.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -138,7 +139,7 @@ public class LoginFragment extends Fragment {
             bodyParams.put("user", params[2]);
             bodyParams.put("password", params[3]);
             String response = httpHandler.makeServiceCall(params[0], params[1], bodyParams, "");
-            if (response != null) {
+            if (response != null && !response.equals("500") ) {
                 try {
                     JSONObject aux = new JSONObject(response);
                     new GlobalPreferences(getActivity().getApplicationContext()).setUser(params[2]);
