15d48a971cf26e334479f965e901aedd555da5a3
Victor Gonzalez <victor.gonzalez.garrido@est.fib.upc.edu>
2017-05-03 21:57:32 +0200

    primer xml fet

--- app/src/main/java/pes/gogreenapp/Fragments/CreateEventFragment.java
+++ app/src/main/java/pes/gogreenapp/Fragments/CreateEventFragment.java
@@ -24,7 +24,7 @@ public class CreateEventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_event, container, false);
+        return inflater.inflate(R.layout.create_event_fragment, container, false);
     }
 
 }
