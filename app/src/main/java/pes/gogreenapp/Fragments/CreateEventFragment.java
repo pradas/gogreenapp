00086c1650fa6ea6811b04ed1fac7902a45bcecd
Victor Gonzalez <victor.gonzalez.garrido@est.fib.upc.edu>
2017-05-06 15:07:19 +0200

    Totes les comprovacions necessaries

--- app/src/main/java/pes/gogreenapp/Fragments/CreateEventFragment.java
+++ app/src/main/java/pes/gogreenapp/Fragments/CreateEventFragment.java
@@ -14,8 +14,10 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -35,9 +37,11 @@ public class CreateEventFragment extends Fragment {
     private EditText DescriptionText;
     private EditText PointsText;
     private EditText DirectionText;
-    private EditText TimeText;
+    private EditText HourText;
+    private EditText MinText;
     private EditText CompanyText;
     private Calendar calendar;
+    private String FinalTime = null;
 
     /**
      * Required empty public constructor
@@ -45,6 +49,34 @@ public class CreateEventFragment extends Fragment {
     public CreateEventFragment() {
     }
 
+    /**
+     * Creates and returns the view hierarchy associated with the fragment.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate any views in
+     *                           the fragment.
+     * @param container          If non-null, this is the parent view that the fragment's UI
+     *                           should be attached to.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous
+     *                           saved state as given here.
+     * @return the View for the fragment's UI, or null.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.create_event_fragment, container, false);
+    }
+
+
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
@@ -56,8 +88,10 @@ public class CreateEventFragment extends Fragment {
         DescriptionText = (EditText) getView().findViewById(R.id.DescriptionCreateEvent_edit_text);
         PointsText = (EditText) getView().findViewById(R.id.PointsCreateEvent_edit_text);
         DirectionText = (EditText) getView().findViewById(R.id.DirectionCreateEvent_edit_text);
-        TimeText = (EditText) getView().findViewById(R.id.TimeCreateEvent_edit_text);
+        HourText = (EditText) getView().findViewById(R.id.HourCreateEvent_edit_text);
+        MinText = (EditText) getView().findViewById(R.id.MinCreateEvent_edit_text);
         CompanyText = (EditText) getView().findViewById(R.id.CompanyCreateEvent_edit_text);
+
         //events
         DateButton.setOnClickListener((View v) -> {
             calendar = Calendar.getInstance();
@@ -88,35 +122,44 @@ public class CreateEventFragment extends Fragment {
             if (DateText.getText().toString().length() <= 0) {
                 DateText.setError("Fecha necesaria");
                 send = false;
+            } else {
+                Date date = null;
+                String inputDate = DateText.getText().toString();
+                try {
+                    DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+                    formatter.setLenient(false);
+                    date = formatter.parse(inputDate);
+                } catch (ParseException e) {
+                    DateText.setError("Formato invalido (dd-mm-yyyy)");
+                }
             }
-            if (TimeText.getText().toString().length() > 0){
-                String[] d = DateText.getText().toString().split(":");
-                Log.d("CreateEvent",d[0]);
-                Log.d("CreateEvent",d[1]);
-                if (d.length == 2){
-                    int hour = Integer.parseInt(d[0]);
-                    int min = Integer.parseInt(d[1]);
-                    if (hour > 23 || min > 59){
-                        TimeText.setError("Mal formato (HH:mm)");
-                    }
+            if (HourText.getText().toString().length() > 0 && MinText.getText().toString().length() <= 0) {
+                MinText.setError("Minutos necesarios");
+                send = false;
+            }
+            if (HourText.getText().toString().length() <= 0 && MinText.getText().toString().length() > 0) {
+                HourText.setError("Hora necesaria");
+
+            }
+            if (HourText.getText().toString().length() > 0 && MinText.getText().toString().length() > 0) {
+                if (Integer.parseInt(HourText.getText().toString()) > 23) {
+                    HourText.setError("Hora incorrecta");
+                    send = false;
                 }
-                else {
-                    TimeText.setError("Mal formato (HH:mm)");
+                if (Integer.parseInt(MinText.getText().toString()) > 59) {
+                    HourText.setError("Minutos incorrectos");
+                    send = false;
                 }
+                FinalTime = HourText.getText().toString() + ":" + MinText.getText().toString();
+                Log.d("CreateEvent", FinalTime);
             }
             if (send) {
+                Log.d("CreateEvent", "se envia");
                 //new LoginFragment.PostLogin().execute("http://10.4.41.145/api/session", "POST",
                 //        textName.getText().toString(), textPassword.getText().toString());
             }
-            // Staring MainActivity
         });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.create_event_fragment, container, false);
-    }
 
 }
