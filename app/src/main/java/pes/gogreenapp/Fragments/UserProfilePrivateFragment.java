b6acc81a15ed43bbf4c7b68960c2bd6d8ea9e893
Albert Díaz <albert.diaz.benitez@fib.upc.edu>
2017-05-06 16:16:54 +0200

    User Profile Private updated with Session Manager Singleton

--- app/src/main/java/pes/gogreenapp/Fragments/UserProfilePrivateFragment.java
+++ app/src/main/java/pes/gogreenapp/Fragments/UserProfilePrivateFragment.java
@@ -83,8 +83,7 @@ public class UserProfilePrivateFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
 
         super.onActivityCreated(savedInstanceState);
-        session = new SessionManager(getActivity().getApplicationContext(),
-                new GlobalPreferences(getActivity().getApplicationContext()).getUser());
+        session = SessionManager.getInstance();
 
         onAttachToParentFragment(getParentFragment());
 
@@ -98,7 +97,7 @@ public class UserProfilePrivateFragment extends Fragment {
         //new GetUserImage().execute("http://ep01.epimg.net/verne/imagenes/2015/09/28/articulo/1443439253_452315_1443439404_sumario_normal.jpg");
 
         initializeUser();
-        userName = session.getUserName();
+        userName = session.getUsername();
         new GetPrivateInfoUser().execute(url + "users/" + userName);
 
         editButton.setOnClickListener(v -> {
