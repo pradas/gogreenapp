888128422ea861610052e185303065992ccd8984
Adrian Borrego <adrian.borrego@est.fib.upc.edu>
2017-05-18 19:01:21 +0200

    Desmarca como favoritos en Rewards Canjeados

--- app/src/main/java/pes/gogreenapp/Adapters/RewardsExchangedAdapter.java
+++ app/src/main/java/pes/gogreenapp/Adapters/RewardsExchangedAdapter.java
@@ -126,6 +126,8 @@ public class RewardsExchangedAdapter extends RecyclerView.Adapter<RewardsExchang
                     holder.fav.setImageResource(R.mipmap.favoritefilled);
                     holder.fav.setTag("favoritefilled");
                 } else {
+                    new DeleteFavorite().execute("http://10.4.41.145/api/users/", "DELETE",
+                            session.getUserName(), holder.id.toString());
                     holder.fav.setImageResource(R.mipmap.favorite);
                     holder.fav.setTag("favorite");
                 }
@@ -174,6 +176,29 @@ public class RewardsExchangedAdapter extends RecyclerView.Adapter<RewardsExchang
                 Toast.makeText(context, "Error al añadir el Reward a favoritos. Intentalo de nuevo mas tarde", Toast.LENGTH_LONG).show();
             }
             else Toast.makeText(context, "Reward añadido a favoritos con exito.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Asynchronous Task for the petition GET of all the Rewards.
+     */
+    private class DeleteFavorite extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpHandler httpHandler = new HttpHandler();
+            HashMap<String, String> bodyParams = new HashMap<>();
+            String url = params[0] + params [2] + "/favourite-rewards/" + params[3];
+            String response = httpHandler.makeServiceCall(url, params[1], bodyParams, session.getToken());
+            if (response != null) return "Correct";
+            return "Error";
+        }
+
+        protected void onPostExecute(String result) {
+            if (result.equalsIgnoreCase("Error")) {
+                Toast.makeText(context, "Error al eliminar el Reward de favoritos. Intentalo de nuevo mas tarde", Toast.LENGTH_LONG).show();
+            }
+            else Toast.makeText(context, "Reward eliminado de favoritos con exito.", Toast.LENGTH_LONG).show();
         }
     }
 }
