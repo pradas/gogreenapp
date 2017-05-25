35cfd89baf1c7269ea16c144e0064308cd5be665
Albert Díaz <albert.diaz.benitez@fib.upc.edu>
2017-05-25 22:08:03 +0200

    javadoc added to new AsyncHttpHandler

--- app/src/main/java/pes/gogreenapp/Utils/AsyncHttpHandler.java
+++ app/src/main/java/pes/gogreenapp/Utils/AsyncHttpHandler.java
@@ -11,6 +11,8 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 /**
+ * Simple class to do the Asynchronous HTTP petitions to the API
+ *
  * @author Albert
  */
 public class AsyncHttpHandler {
@@ -18,29 +20,60 @@ public class AsyncHttpHandler {
     private static final String BASE_URL = "http://10.4.41.145/api/";
     private static AsyncHttpClient client = new AsyncHttpClient();
 
+    /**
+     * Perform a GET petition to the API
+     *
+     * @param url             url + BASE_URL are the uri HTTP petition
+     * @param params          body params of the petition, it can be null in case of empty body
+     * @param responseHandler AsyncHttpResponseHandler to create a thread
+     */
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
 
         setToken();
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
+    /**
+     * Perform a POST petition to the API
+     *
+     * @param url             url + BASE_URL are the uri HTTP petition
+     * @param params          body params of the petition, it can be null in case of empty body
+     * @param responseHandler AsyncHttpResponseHandler to create a thread
+     */
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
 
         setToken();
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
+    /**
+     * Perform a PUT petition to the API
+     *
+     * @param url             url + BASE_URL are the uri HTTP petition
+     * @param params          body params of the petition, it can be null in case of empty body
+     * @param responseHandler AsyncHttpResponseHandler to create a thread
+     */
     public static void put(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
 
         setToken();
         client.put(getAbsoluteUrl(url), params, responseHandler);
     }
 
+    /**
+     * Join the BASE_URL and relativeUrl
+     *
+     * @param relativeUrl url relative to the BASE_URL
+     *
+     * @return a String resulting of the concatenation of BASE_URL and relativeUrl
+     */
     private static String getAbsoluteUrl(String relativeUrl) {
 
         return BASE_URL + relativeUrl;
     }
 
+    /**
+     * Set the token authentication to the client
+     */
     private static void setToken() {
 
         client.addHeader("Authorization", "Bearer {" + SessionManager.getInstance().getToken() + "}");
