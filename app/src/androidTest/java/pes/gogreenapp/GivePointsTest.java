f2504a07723c2a68b10f98707d3ebc312b81763c
Adrian Borrego <adrian.borrego@est.fib.upc.edu>
2017-06-01 20:32:11 +0200

    Tests de dar puntos completados

--- app/src/androidTest/java/pes/gogreenapp/GivePointsTest.java
+++ app/src/androidTest/java/pes/gogreenapp/GivePointsTest.java
@@ -1,28 +1,34 @@
 package pes.gogreenapp;
 
 import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import pes.gogreenapp.Activities.MainActivity;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.EasyMock2Matchers.equalTo;
 
 /**
  * Created by Adrian on 20/05/2017.
@@ -141,7 +147,7 @@ public class GivePointsTest {
      */
     @Test
     public void clickSwitchDisplaysAlertDialogWithText() {
-        onView(withId(R.id.switchModeItem)).perform(click());
+        onView(withId(R.id.switchModeItem)).check(matches(isNotChecked())).perform(click());
         onView(withText("¿Está seguro que desea cambiar el modo de entregar puntos? Se perderán los datos introducidos"))
                 .check(matches(isDisplayed()));
     }
@@ -151,7 +157,7 @@ public class GivePointsTest {
      */
     @Test
     public void clickSwitchDisplaysAlertDialogWithAcceptButton() {
-        onView(withId(R.id.switchModeItem)).perform(click());
+        onView(withId(R.id.switchModeItem)).check(matches(isNotChecked())).perform(click());
         onView(withText("ACEPTAR")).check(matches(isDisplayed()));
     }
 
@@ -160,7 +166,7 @@ public class GivePointsTest {
      */
     @Test
     public void clickSwitchDisplaysAlertDialogWithCancelButton() {
-        onView(withId(R.id.switchModeItem)).perform(click());
+        onView(withId(R.id.switchModeItem)).check(matches(isNotChecked())).perform(click());
         onView(withText("CANCELAR")).check(matches(isDisplayed()));
     }
 
@@ -207,5 +213,63 @@ public class GivePointsTest {
         onView(withId(R.id.switchModeItem)).check(matches(isChecked()));
         onView(withId(R.id.listViewGivePoints)).check(matches(hasDescendant(withHint("Puntos"))));
     }
+
+    /**
+     * Check if the xml have the switch button
+     */
+    @Test
+    public void anotherUserDisplaysAnotherItem() {
+        onView(withId(R.id.anotherUserToGive)).perform(click());
+        onView(withText("Usuario nº2")).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Check if the xml have the switch button
+     */
+    @Test
+    public void clickGrantPointsDisplaysAlertDialogWithText() {
+        onView(withId(R.id.grantPointsToUsers)).perform(click());
+        onView(withText("¿Está seguro que desea entregar los puntos a esos usuarios?"))
+                .check(matches(isDisplayed()));
+    }
+
+    /**
+     * Check if the xml have the switch button
+     */
+    @Test
+    public void clickGrantPointsDisplaysAlertDialogWithGrantButton() {
+        onView(withId(R.id.grantPointsToUsers)).perform(click());
+        onView(withText("ENTREGAR PUNTOS")).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Check if the xml have the switch button
+     */
+    @Test
+    public void clickGrantPointsDisplaysAlertDialogWithCancelButton() {
+        onView(withId(R.id.grantPointsToUsers)).perform(click());
+        onView(withText("CANCELAR")).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Check if the xml have the switch button
+     */
+    @Test
+    public void clickGrantPointsAndAcceptDisplaysSameLayout() {
+        onView(withId(R.id.grantPointsToUsers)).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(R.id.layoutGivePoints)).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Check if the xml have the switch button
+     */
+    @Test
+    public void clickGrantPointsAndCancelDisplaysSameLayout() {
+        onView(withId(R.id.grantPointsToUsers)).perform(click());
+        onView(withId(android.R.id.button2)).perform(click());
+        onView(withId(R.id.layoutGivePoints)).check(matches(isDisplayed()));
+    }
+
 
 }
