21e402f45e8407770e73ff6a163edeb38ff01970
Albert Díaz <albert.diaz.benitez@fib.upc.edu>
2017-06-06 17:34:05 +0200

    start feature78

---
+++ app/src/main/java/pes/gogreenapp/Adapters/EmployeeListAdapter.java
@@ -0,0 +1,11 @@
+package pes.gogreenapp.Adapters;
+
+/**
+ * All right reserverd to GoBros Devevelopers team.
+ * This code is free software; you can redistribute it and/or modify itunder the terms of
+ * the GNU General Public License version 2 only, as published by the Free Software Foundation.
+ */
+
+public class EmployeeListAdapter {
+
+}
