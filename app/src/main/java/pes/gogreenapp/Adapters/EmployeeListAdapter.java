2195719b00c08ef1f765cdf9fb4e402f6b4c6e37
Albert Díaz <albert.diaz.benitez@fib.upc.edu>
2017-06-06 18:11:13 +0200

    GET /shop/<id>/employees done

--- app/src/main/java/pes/gogreenapp/Adapters/EmployeeListAdapter.java
+++ app/src/main/java/pes/gogreenapp/Adapters/EmployeeListAdapter.java
@@ -1,11 +1,61 @@
 package pes.gogreenapp.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pes.gogreenapp.R;
+
 /**
  * All right reserverd to GoBros Devevelopers team.
  * This code is free software; you can redistribute it and/or modify itunder the terms of
  * the GNU General Public License version 2 only, as published by the Free Software Foundation.
  */
 
-public class EmployeeListAdapter {
+public class EmployeeListAdapter extends BaseAdapter implements ListAdapter {
 
+    private List<String> employees = new ArrayList<>();
+    private Context context;
+
+    public EmployeeListAdapter(List<String> employees, Context context) {
+
+        this.employees = employees;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+
+        return employees.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+
+        return employees.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.employee_manager_listview, parent, false);
+        }
+
+        return view;
+    }
 }
--- app/src/main/java/pes/gogreenapp/Fragments/EmployeeManagerFragment.java
+++ app/src/main/java/pes/gogreenapp/Fragments/EmployeeManagerFragment.java
@@ -2,12 +2,28 @@ package pes.gogreenapp.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+import pes.gogreenapp.Adapters.EmployeeListAdapter;
 import pes.gogreenapp.R;
+import pes.gogreenapp.Utils.AsyncHttpHandler;
+import pes.gogreenapp.Utils.SessionManager;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,5 +41,42 @@ public class EmployeeManagerFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.employee_manager_fragment, container, false);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+        SessionManager instance = SessionManager.getInstance();
+
+        List<String> employees = new ArrayList<>();
+        AsyncHttpHandler.get("shop/" + instance.getShopId() + "/employees", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // Handle resulting parsed JSON response here
+                try {
+                    JSONArray jsonArray = response.getJSONArray("employees");
+                    for (int i = 0; i < jsonArray.length(); ++i) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String employee = jsonObject.getString("username");
+                        employees.add(employee);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject response) {
+                // called when response HTTP status is "4XX"
+                Log.e("API_ERROR", String.valueOf(statusCode) + " " + response.toString());
+            }
+        });
+
+        // create the adapter and instance the content of the list view
+        EmployeeListAdapter adapter = new EmployeeListAdapter(employees, getActivity().getApplicationContext());
+        ListView listView = (ListView) getView().findViewById(R.id.list_employees);
+        listView.setAdapter(adapter);
+    }
+
 
 }
