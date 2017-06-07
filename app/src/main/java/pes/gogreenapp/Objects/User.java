fd062a632d6a38b275e5443c2b9f576265640d7a
Adrian Borrego <adrian.borrego@est.fib.upc.edu>
2017-06-07 17:23:21 +0200

    Objects documentados

--- app/src/main/java/pes/gogreenapp/Objects/User.java
+++ app/src/main/java/pes/gogreenapp/Objects/User.java
@@ -21,13 +21,29 @@ public class User {
     private Date creationDate;
     private String userUrlImage;
 
-    // fields of User table on gogreen.db
+    /**
+     * fields of User table on gogreen.db
+      */
     private String role;
     private String token;
 
-    //Calendar cal = GregorianCalendar.getInstance();
+    /**
+     * Calendar cal = GregorianCalendar.getInstance();
+     */
     private DateFormat sourceFormat = new SimpleDateFormat("dd-MM-yyyy");
 
+    /**
+     * Constructor of the Shop Object.
+     *
+     * @param username       of the User
+     * @param name    of the User.
+     * @param email   of the User.
+     * @param birthDate  of the User.
+     * @param userUrlImage       of the User.
+     * @param totalPoints    of the User.
+     * @param currentPoints   of the User.
+     * @param creationDate  of the User.
+     */
     public User(String username, String name, String email, String birthDate, String userUrlImage, Integer totalPoints,
                 Integer currentPoints, String creationDate) {
 
@@ -49,6 +65,17 @@ public class User {
         this.userUrlImage = userUrlImage;
     }
 
+    /**
+     * Constructor of the Shop Object.
+     *
+     * @param username       of the User
+     * @param name    of the User.
+     * @param email   of the User.
+     * @param birthDate  of the User.
+     * @param userUrlImage       of the User.
+     * @param totalPoints    of the User.
+     * @param currentPoints   of the User.
+     */
     public User(String username, String name, String email, String birthDate, String userUrlImage, int totalPoints,
                 int currentPoints) {
 
@@ -83,101 +110,201 @@ public class User {
         this.shopId = shopId;
     }
 
+    /**
+     * Getter of the User username
+     *
+     * @return the User username
+     */
     public String getUsername() {
 
         return username;
     }
 
+    /**
+     * Setter of the User username.
+     *
+     * @param username new username of the User.
+     */
     public void setUsername(String username) {
 
         this.username = username;
     }
 
+    /**
+     * Getter of the User name
+     *
+     * @return the User name
+     */
     public String getName() {
 
         return name;
     }
 
+    /**
+     * Setter of the User name.
+     *
+     * @param name new username of the User.
+     */
     public void setName(String name) {
 
         this.name = name;
     }
 
+    /**
+     * Getter of the User email
+     *
+     * @return the User email
+     */
     public String getEmail() {
 
         return email;
     }
 
+    /**
+     * Setter of the User email.
+     *
+     * @param email new email of the User.
+     */
     public void setEmail(String email) {
 
         this.email = email;
     }
 
+    /**
+     * Getter of the User current points
+     *
+     * @return the User current points
+     */
     public Integer getCurrentPoints() {
 
         return currentPoints;
     }
 
+    /**
+     * Setter of the User current points.
+     *
+     * @param currentPoints new current points of the User.
+     */
     public void setCurrentPoints(Integer currentPoints) {
 
         this.currentPoints = currentPoints;
     }
 
+    /**
+     * Getter of the User total points
+     *
+     * @return the User total points
+     */
     public Integer getTotalPoints() {
 
         return totalPoints;
     }
 
+    /**
+     * Setter of the User total points.
+     *
+     * @param totalPoints new total points of the User.
+     */
     public void setTotalPoints(Integer totalPoints) {
 
         this.totalPoints = totalPoints;
     }
 
+    /**
+     * Getter of the User birth date
+     *
+     * @return the User birth date
+     */
     public Date getBirthDate() {
 
         return birthDate;
     }
 
+    /**
+     * Setter of the User birth date.
+     *
+     * @param birthDate new birthDate of the User.
+     */
     public void setBirthDate(Date birthDate) {
 
         this.birthDate = birthDate;
     }
 
+    /**
+     * Getter of the User creation date
+     *
+     * @return the User creation date
+     */
     public Date getCreationDate() {
 
         return creationDate;
     }
 
+    /**
+     * Setter of the User creationDate.
+     *
+     * @param creationDate new creationDate of the User.
+     */
     public void setCreationDate(Date creationDate) {
 
         this.creationDate = creationDate;
     }
 
+    /**
+     * Getter of the User url image
+     *
+     * @return the User url image
+     */
     public String getUserUrlImage() {
 
         return userUrlImage;
     }
 
+    /**
+     * Setter of the User url image.
+     *
+     * @param userUrlImage new urlImage of the User.
+     */
     public void setUserUrlImage(String userUrlImage) {
 
         this.userUrlImage = userUrlImage;
     }
 
+    /**
+     * Getter of the User role
+     *
+     * @return the User role
+     */
     public String getRole() {
 
         return role;
     }
 
+    /**
+     * Getter of the User token
+     *
+     * @return the User token
+     */
     public String getToken() {
 
         return token;
     }
 
+    /**
+     * Getter of the User shop id
+     *
+     * @return the User shop id
+     */
     public Integer getShopId() {
 
         return shopId;
     }
 
+    /**
+     * Setter of the User shop id.
+     *
+     * @param shopId new shopId of the User.
+     */
     public void setShopId(Integer shopId) {
 
         this.shopId = shopId;
